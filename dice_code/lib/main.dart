6421784699dc4141f13e46f81312612369d99ffa
Maryammohd-hub <maryammohd.aseeri@gmail.com>
2026-03-21 01:59:52 +0300

    added some functions

--- dice_code/lib/main.dart
+++ dice_code/lib/main.dart
@@ -1,12 +1,21 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+int leftDiceNumber = 1;
+int rightDiceNumber = 1;
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,15 +27,25 @@ class MyApp extends StatelessWidget {
           child: Row(
             children: [
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Image.asset('images/dice1.png'),
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      leftDiceNumber = Random().nextInt(6) + 1;
+                      rightDiceNumber = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'),
                 ),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Image.asset('images/dice1.png'),
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      leftDiceNumber = Random().nextInt(6) + 1;
+                      rightDiceNumber = Random().nextInt(6) + 1;
+                    });
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png'),
                 ),
               ),
             ],
